762203abda39001b9675abdf1d5a96b99af703c4
rickyanjasmaraa <rickyanjasmaraa@gmail.com>
2024-06-05 09:38:23 +0700

    config all

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-//ini javascript
+//FUNGSI JAVACRIPT PADA MENU TOGGLE, KETIKA UKURAN BERUBAH AKAN MENAMPILKAN MENU NAV YANG DISEMBUNYIKAN DAN BISA DI KLIK
+
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle ('open');
+}
